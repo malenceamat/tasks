30117b00286c3ce76181b7d355ddeb777e8e69c8
malenceamat <yatikivan@gmail.com>
2026-05-21 02:02:32 +0300

    тестовое задание

--- local/components/corporate/car.list/.description.php
+++ local/components/corporate/car.list/.description.php
@@ -2,8 +2,8 @@
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
 $arComponentDescription = [
-    'NAME'        => 'Список доступных автомобилей',
+    'NAME' => 'Список доступных автомобилей',
     'DESCRIPTION' => 'Выводит список свободных автомобилей для текущего сотрудника',
-    'CACHE_PATH'  => 'Y',
-    'SORT'        => 10,
+    'CACHE_PATH' => 'Y',
+    'SORT' => 10,
 ];
